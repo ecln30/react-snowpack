9d8792608d2ea7cb90c5cffc449de663147ad2f5
ecln30 <ecln30@gmail.com>
2022-09-03 17:21:42 +0900

    add stopbell

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
 import './App.css'
 import logo from './logo.svg'
-import street from './assets/street.png'
+import stop from './stop.mp3'
+import reset from './reset.mp3'
 import React, {useState, useEffect} from 'react';
 import Main from './components/Main.jsx'
 
 // App component
 function App() {
   const [count, setCount] = useState(0);
+  const [text,setText] = useState('')
+  const [istrue,setIstrue] = useState(false)
+  const [post,setPost] = useState({
+    aboutYou: text,name:name,id: Date.now() })
+
+  // audio
+  const stopbell = new Audio(stop)
+  const resetbell = new Audio(reset)
+
+  function handlePost() {
+
+  }
   useEffect(() => {
+    if(istrue) return
     const timer = setTimeout(f => setCount(count + 1), 1000);
     return f => clearTimeout(timer);
   }, [count, setCount]);
@@ -15,8 +29,14 @@ function App() {
   return (
     <div className="App">
        <h1 className='title'>Hello world</h1>
-       <p>can we go further</p>
-       <Main />
+       <Main count={count} setCount={setCount} 
+        handlePost={handlePost}
+        setText={setText}
+        setIstrue ={setIstrue}
+        text={text}
+        stopbell={stopbell}
+        resetbell={resetbell}
+       />
     </div>
   );
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -14,11 +14,37 @@
 
 import React from 'react'
 
-function Main() {
+function Main({count,setCount,text,setText,handlePost,setIstrue,stopbell,resetbell}) {
+
   return (
-    <div>
-        This is main 
-    </div>
+    <div className='main'>
+     <h1 className='text'>This is main </h1>
+     <h2 className='tell'>Tell the World ABOUT YOU</h2>
+     <button className='post-btn'
+      onClick={handlePost}
+     >Post</button>
+     <textarea className='txtBox'
+      value={text} onChange={ e => setText(e.target.value)}
+     >
+     </textarea>
+     <p className='count'>{count}</p>
+     <div>
+
+     <button 
+      onClick={f =>{
+      setCount(0);
+      setIstrue(false)
+      resetbell.play()
+      }}
+     className='reset-btn'>reset</button>
+      &nbsp;&nbsp;&nbsp;
+     <button 
+      onClick={f => {setIstrue(true);stopbell.play()}}
+      className='stop-btn'>stop</button>
+     </div>
+
+  </div>
+
   )
 }
 
